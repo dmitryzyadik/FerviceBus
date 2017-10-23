f1621af17e101fd72f3ed196f3f338bde04a7527
Dmirty Zyadik <zyadik@mail.ru>
2017-10-23 16:14:21 +0500

    Rewrited all code

--- ConsoleApplication2/Program.fs
+++ ConsoleApplication2/Program.fs
@@ -6,40 +6,55 @@ open System.Data
 open System.Data.Linq
 open FSharp.Data.TypeProviders
 open Microsoft.FSharp.Linq
+open FSharp.Data.JsonExtensions
 
-let routesId = [6,7,8,9,10,11,12,13,14,15,16,17,18]
-type BusRoute = JsonProvider<("http://www.patp2-nv.ru/monitoring/route/routeinfo.php?a=GetRouteVehicles&id=7")>
-type dbSchema3 = SqlDataConnection<"Data Source=SQLBACK-WIN12;Initial Catalog=Dima;Integrated Security=SSPI;uid=BusDriver;pwd=klop;">
+
+let routesId:seq<int> =  seq {for i in 6 .. 18 do yield i} 
+let routeUrl:string = "http://www.patp2-nv.ru/monitoring/route/routeinfo.php?a=GetRouteVehicles&id="
+type dbSchema1 = SqlDataConnection<"Data Source=SQLBACK-WIN12;Initial Catalog=Dima;Integrated Security=SSPI;uid=BusDriver;pwd=klop;">
+
+let createRouteUrls (url:string) (routesId:seq<int>) : (seq<string>)  =    
+        routesId |> Seq.map(fun x -> sprintf "%s"  url + x.ToString())
+    
+
+let getListRoutesUrl = createRouteUrls  routeUrl routesId    
+
+let loadRoute (url:string) =
+    let valueAsync = 
+        JsonValue.Load(url)
+    valueAsync
+
+let db = dbSchema1.GetDataContext() 
+    
+        
+let loadRoutes=         
+            getListRoutesUrl |> Seq.map(fun x -> loadRoute x)
+        
+let parse jsonRoute =
+        match jsonRoute with
+        | JsonValue.Record [| info; data |] -> 
+            let num = match info with
+                      | s ,d -> d?routeId.AsString()       
+            
+            match data with
+            |  s, d-> 
+                //let vehicles = d?vehicles
+                [
+                for v in d -> new dbSchema1.ServiceTypes.BusLocation(RouteNum = "6", 
+                                                                            BusNum = v?public_number.AsString(), 
+                                                                            Lat = (float32)(v?lat.AsFloat()), 
+                                                                            Lon = (float32)(v?lon.AsFloat()), 
+                                                                            DataTime = DateTime.Now)
+                ]
+
 [<EntryPoint>]
-let main argv = 
-    
-    let id = "6"
-    let getRoute id = "http://www.patp2-nv.ru/monitoring/route/routeinfo.php?a=GetRouteVehicles&id=" + id
-    let show id = "http://www.patp2-nv.ru/monitoring/route/routeinfo.php?a=Show&id=" + id
-    
-    let r = 
-        BusRoute.AsyncLoad((getRoute id))
+let main argv =  
 
-    let s = BusRoute.GetSample()
-
-    let db = dbSchema3.GetDataContext()
+    loadRoutes |> Seq.iter(fun x  -> db.BusLocation.InsertAllOnSubmit(parse x))
     
-    let newRecord = [ for b in s.Vehicles  -> new dbSchema3.ServiceTypes.BusLocation(RouteNum = id, 
-                                                            BusNum = b.PublicNumber, 
-                                                            Lan = (float32)b.Lat, 
-                                                            Lin = (float32)b.Lon, 
-                                                            DataTime = DateTime.Now)
-                                                            ]
-    
-    db.BusLocation.InsertAllOnSubmit(newRecord)
     try
         db.DataContext.SubmitChanges()
-    with
+    with    
     | exn -> printfn "Exception:\n%s" exn.Message
-    
-    //for  v in s.Vehicles do
-        
-    
-    
     printfn "%A" argv
     0 // return an integer exit code
